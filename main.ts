d9c59cde61e42b9882e1988df12b1b7229e3c6b8
IvoKuban <146112708+IvoKuban@users.noreply.github.com>
2025-02-11 12:58:54 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(kroky)
+})
+input.onGesture(Gesture.Shake, function () {
+    kroky += 1
+})
+let kroky = 0
+kroky = 0
+basic.forever(function () {
+	
+})
